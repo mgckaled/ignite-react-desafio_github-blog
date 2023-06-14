77cd7a6f5b5606e4882deb2597510a6e70f7768a
mgckaled <marcelgckaled@gmail.com>
2023-06-14 19:05:53 -0300

    feat: create Post component for Blog page

---
+++ src/pages/Blog/components/Post/index.tsx
@@ -0,0 +1,17 @@
+import { PostContainer } from "./styles"
+
+export function Post() {
+  return (
+    <PostContainer to='#'>
+      <div>
+        <strong>JavaScript data types and data structures</strong>
+        <span>Há 1 dia</span>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit aspernatur reiciendis omnis voluptatum ex
+        reprehenderit, optio aliquam illo natus suscipit, dolorem doloremque est, enim quos saepe minus! Cupiditate,
+        aspernatur adipisci.
+      </p>
+    </PostContainer>
+  )
+}
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,24 @@
-import { Profile } from "./components/Profile";
-import { SearchInput } from "./components/SearchInput";
+import { Post } from "./components/Post"
+import { Profile } from "./components/Profile"
+import { SearchInput } from "./components/SearchInput"
+import { PostsListContainer } from "./styles"
 
 export function Blog() {
   return (
     <>
       <Profile />
       <SearchInput />
+      <PostsListContainer>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </PostsListContainer>
     </>
   )
 }
